1e97999ab1478433dd1f55bb0f45735c2a4421e4
Luc Fourestier <luc.fourestier@premiumsoundsolutions.com>
2015-09-08 12:56:25 +0200

    typo

--- Example/Test/Run.bat
+++ Example/Test/Run.bat
@@ -1,6 +1,6 @@
 # Windows
 
-python ..\..\Scripts/RunTest.py -d Pytest -c Commands.cfg -r TestReport.csv -s TestSynthesis.csv
+python ..\..\Scripts\RunTest.py -d Pytest -c Commands.cfg -r TestReport.csv -s TestSynthesis.csv
 
 # Full command
-#python ..\..\Scripts/RunTest.py -d Pytest,Cunit,Gtest,Junit,Cppuint -c Commands.cfg -r TestReport.csv -s TestSynthesis.csv
+#python ..\..\Scripts\RunTest.py -d Pytest,Cunit,Gtest,Junit,Cppuint -c Commands.cfg -r TestReport.csv -s TestSynthesis.csv
